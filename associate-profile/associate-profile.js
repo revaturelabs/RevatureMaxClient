c87e02b01732cda3e51ea491b85d30bd67ab789b
Marcus Kee <62085649+keemarcus@users.noreply.github.com>
2021-07-29 16:10:52 -0400

    got forms working (with mocked backend)

--- associate-profile/associate-profile.js
+++ associate-profile/associate-profile.js
@@ -1,3 +1,4 @@
+// get all the page elements we need to manipulate
 const editContactInfo = document.getElementById("editContactInfo")
 const cancelContactInfo = document.getElementById("cancelContactInfo")
 const submitContactInfo = document.getElementById("submitContactInfo")
@@ -7,48 +8,198 @@ const submitBio = document.getElementById("submitBio")
 const editFavTechnologies = document.getElementById("editFavTechnologies")
 const cancelFavTechnologies = document.getElementById("cancelFavTechnologies")
 const submitFavTechnologies = document.getElementById("submitFavTechnologies")
-
 const firstName = document.getElementById("userFirstName")
 const lastName = document.getElementById("userLastName")
 const email = document.getElementById("userEmail")
+const bio = document.getElementById("userBio")
+const tech1 = document.getElementById("tech1")
+const tech2 = document.getElementById("tech2")
+const tech3 = document.getElementById("tech3")
+const preference = document.getElementById("userPreference")
+const contactInfoForm = document.getElementById("updateContactInfo")
+const bioForm = document.getElementById("updateBio")
+const favTechnologiesForm = document.getElementById("updateFavTechnologies")
+const newFirstName = document.getElementById("editFirstName")
+const newLastName = document.getElementById("editLastName")
+const newEmail = document.getElementById("editEmail")
+const newBio = document.getElementById("newBio")
+const newTech1 = document.getElementById("editTech1")
+const newTech2 = document.getElementById("editTech2")
+const newTech3 = document.getElementById("editTech3")
+const newPreference = document.getElementById("editPreference")
 
-editContactInfo.onclick = function(){
-    const editFirstName = document.getElementById("editFirstName")
-    const editLastName = document.getElementById("editLastName")
-    const editEmail = document.getElementById("editEmail")
-    this.style.display = "none"
+// setup event handlers for buttons
+editContactInfo.onclick = setupContactInfoForm
+cancelContactInfo.onclick = resetContactInfoForm
+editBio.onclick = setupBioForm
+cancelBio.onclick = resetBioForm
+editFavTechnologies.onclick = setupFavTechnologiesForm
+cancelFavTechnologies.onclick = resetFavTechnologiesForm
+
+// make session variable for user
+const currentUser = 1
+
+// get the info for the current user
+getAssociateInfo(currentUser)
+
+contactInfoForm.onsubmit = function (e) {
+    // stop the regular form submission
+    e.preventDefault()
+
+    let newInfo = {}
+
+    // get the info from the form that we submitted
+    newInfo["firstName"] = this["firstName"].value
+    newInfo["lastName"] = this["lastName"].value
+    newInfo["emailAddress"] = this["emailAddress"].value
+
+    // get the rest of the info from the elements on the page
+    newInfo["bio"] = bio.innerText
+    newInfo["favoriteTechnologies"] = [tech1.innerText, tech2.innerText, tech3.innerText]
+    newInfo["preference"] = preference.innerText
+
+    // construct an HTTP request
+    let xhr = new XMLHttpRequest()
+    xhr.open(this.method, this.action, true)
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
+
+    // send the info as JSON
+    xhr.send(JSON.stringify(newInfo))
+
+    xhr.onloadend = function () {
+        // update the info on the page
+        getAssociateInfo(currentUser)
+
+        // reset the form elements
+        resetContactInfoForm()
+    }
+}
+
+bioForm.onsubmit = function (e) {
+    // stop the regular form submission
+    e.preventDefault()
+
+    let newInfo = {}
+
+    // get the info from the form that we submitted
+    newInfo["bio"] = this["bio"].value
+    
+    // get the rest of the info from the elements on the page
+    newInfo["firstName"] = firstName.innerText
+    newInfo["lastName"] = lastName.innerText
+    newInfo["emailAddress"] = email.innerText
+    newInfo["favoriteTechnologies"] = [tech1.innerText, tech2.innerText, tech3.innerText]
+    newInfo["preference"] = preference.innerText
+
+    // construct an HTTP request
+    let xhr = new XMLHttpRequest()
+    xhr.open(this.method, this.action, true)
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
+
+    // send the info as JSON
+    xhr.send(JSON.stringify(newInfo))
+
+    xhr.onloadend = function () {
+        // update the info on the page
+        getAssociateInfo(currentUser)
+
+        // reset the form elements
+        resetBioForm()
+    }
+}
+
+favTechnologiesForm.onsubmit = function (e) {
+    // stop the regular form submission
+    e.preventDefault()
+
+    let newInfo = {}
+
+    // get the info from the form that we submitted
+    newInfo["favoriteTechnologies"] = [this["tech1"].value, this["tech2"].value, this["tech3"].value]
+    newInfo["preference"] = this["preference"].value
+
+    // get the rest of the info from the elements on the page
+    newInfo["firstName"] = firstName.innerText
+    newInfo["lastName"] = lastName.innerText
+    newInfo["emailAddress"] = email.innerText
+    newInfo["bio"] = bio.innerText
+    
+
+    // construct an HTTP request
+    let xhr = new XMLHttpRequest()
+    xhr.open(this.method, this.action, true)
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
+
+    // send the info as JSON
+    xhr.send(JSON.stringify(newInfo))
+
+    xhr.onloadend = function () {
+        // update the info on the page
+        getAssociateInfo(currentUser)
+
+        // reset the form elements
+        resetFavTechnologiesForm()
+    }
+}
+
+async function getAssociateInfo(id) {
+    let url = "http://localhost:5000/associate"
+    let response = await fetch(url)
+    let associateInfo = await response.json()
+    firstName.innerText = associateInfo["firstName"]
+    newFirstName.value = associateInfo["firstName"]
+    lastName.innerText = associateInfo["lastName"]
+    newLastName.value = associateInfo["lastName"]
+    email.innerText = associateInfo["emailAddress"]
+    newEmail.value = associateInfo["emailAddress"]
+    bio.innerText = associateInfo["bio"]
+    bio.style.height = "fit-content"
+    newBio.value = associateInfo["bio"]
+    let favTechs = associateInfo["favoriteTechnologies"]
+    tech1.innerText = favTechs[0]
+    newTech1.value = favTechs[0]
+    tech2.innerText = favTechs[1]
+    newTech2.value = favTechs[1]
+    tech3.innerText = favTechs[2]
+    newTech3.value = favTechs[2]
+    preference.innerText = associateInfo["preference"]
+    newPreference.value = associateInfo["preference"]
+}
+
+function menuToggle() {
+    const toggleMenu = document.querySelector('.menu');
+    toggleMenu.classList.toggle('active');
+}
+
+function setupContactInfoForm() {
+    editContactInfo.style.display = "none"
     cancelContactInfo.style.display = "inline"
     submitContactInfo.style.display = "inline"
     firstName.style.display = "none"
-    editFirstName.style.display = "inline"
-    editFirstName.value = firstName.innerText
+    newFirstName.style.display = "inline"
+    newFirstName.value = firstName.innerText
     lastName.style.display = "none"
-    editLastName.style.display = "inline"
-    editLastName.value = lastName.innerText
+    newLastName.style.display = "inline"
+    newLastName.value = lastName.innerText
     email.style.display = "none"
-    editEmail.style.display = "inline"
-    editEmail.value = email.innerText
+    newEmail.style.display = "inline"
+    newEmail.value = email.innerText
 }
 
-cancelContactInfo.onclick = function(){
-    const editFirstName = document.getElementById("editFirstName")
-    const editLastName = document.getElementById("editLastName")
-    const editEmail = document.getElementById("editEmail")
-    this.style.display = "none"
+function resetContactInfoForm() {
+    cancelContactInfo.style.display = "none"
     editContactInfo.style.display = "inline"
     submitContactInfo.style.display = "none"
     firstName.style.display = "inline"
-    editFirstName.style.display = "none"
+    newFirstName.style.display = "none"
     lastName.style.display = "inline"
-    editLastName.style.display = "none"
+    newLastName.style.display = "none"
     email.style.display = "inline"
-    editEmail.style.display = "none"
+    newEmail.style.display = "none"
 }
 
-editBio.onclick = function(){
-    const bio = document.getElementById("userBio")
-    const newBio = document.getElementById("newBio")
-    this.style.display = "none"
+function setupBioForm() {
+    editBio.style.display = "none"
     submitBio.style.display = "inline"
     cancelBio.style.display = "inline"
     bio.style.display = "none"
@@ -56,37 +207,15 @@ editBio.onclick = function(){
     newBio.innerText = bio.innerText
 }
 
-cancelBio.onclick = function(){
-    const bio = document.getElementById("userBio")
-    const newBio = document.getElementById("newBio")
-    this.style.display = "none"
+function resetBioForm() {
+    cancelBio.style.display = "none"
     submitBio.style.display = "none"
     editBio.style.display = "inline"
     bio.style.display = "block"
     newBio.style.display = "none"
 }
 
-submitBio.onclick = function(){
-    const bio = document.getElementById("userBio")
-    const newBio = document.getElementById("newBio")
-    this.style.display = "none"
-    cancelBio.style.display = "none"
-    editBio.style.display = "inline"
-    bio.style.display = "block"
-    bio.innerText = newBio.value
-    bio.style.height = "fit-content"
-    newBio.style.display = "none"
-}
-
-editFavTechnologies.onclick = function(){
-    const tech1 = document.getElementById("tech1")
-    const editTech1 = document.getElementById("editTech1")
-    const tech2 = document.getElementById("tech2")
-    const editTech2 = document.getElementById("editTech2")
-    const tech3 = document.getElementById("tech3")
-    const editTech3 = document.getElementById("editTech3")
-    const preference = document.getElementById("userPreference")
-    const editPreference = document.getElementById("editPreference")
+function setupFavTechnologiesForm() {
     editFavTechnologies.style.display = "none"
     submitFavTechnologies.style.display = "inline"
     cancelFavTechnologies.style.display = "inline"
@@ -94,25 +223,17 @@ editFavTechnologies.onclick = function(){
     tech2.style.display = "none"
     tech3.style.display = "none"
     preference.style.display = "none"
-    editTech1.style.display = "block"
-    editTech1.value = tech1.innerText
-    editTech2.style.display = "block"
-    editTech2.value = tech2.innerText
-    editTech3.style.display = "block"
-    editTech3.value = tech3.innerText
-    editPreference.style.display = "inline"
-    editPreference.value = preference.innerText
+    newTech1.style.display = "block"
+    newTech1.value = tech1.innerText
+    newTech2.style.display = "block"
+    newTech2.value = tech2.innerText
+    newTech3.style.display = "block"
+    newTech3.value = tech3.innerText
+    newPreference.style.display = "inline"
+    newPreference.value = preference.innerText
 }
 
-cancelFavTechnologies.onclick = function(){
-    const tech1 = document.getElementById("tech1")
-    const editTech1 = document.getElementById("editTech1")
-    const tech2 = document.getElementById("tech2")
-    const editTech2 = document.getElementById("editTech2")
-    const tech3 = document.getElementById("tech3")
-    const editTech3 = document.getElementById("editTech3")
-    const preference = document.getElementById("userPreference")
-    const editPreference = document.getElementById("editPreference")
+function resetFavTechnologiesForm() {
     editFavTechnologies.style.display = "inline"
     submitFavTechnologies.style.display = "none"
     cancelFavTechnologies.style.display = "none"
@@ -120,35 +241,8 @@ cancelFavTechnologies.onclick = function(){
     tech2.style.display = "block"
     tech3.style.display = "block"
     preference.style.display = "inline"
-    editTech1.style.display = "none"
-    editTech2.style.display = "none"
-    editTech3.style.display = "none"
-    editPreference.style.display = "none"
+    newTech1.style.display = "none"
+    newTech2.style.display = "none"
+    newTech3.style.display = "none"
+    newPreference.style.display = "none"
 }
-
-submitFavTechnologies.onclick = function(){
-    const tech1 = document.getElementById("tech1")
-    const editTech1 = document.getElementById("editTech1")
-    const tech2 = document.getElementById("tech2")
-    const editTech2 = document.getElementById("editTech2")
-    const tech3 = document.getElementById("tech3")
-    const editTech3 = document.getElementById("editTech3")
-    const preference = document.getElementById("userPreference")
-    const editPreference = document.getElementById("editPreference")
-    editFavTechnologies.style.display = "inline"
-    submitFavTechnologies.style.display = "none"
-    cancelFavTechnologies.style.display = "none"
-    tech1.style.display = "block"
-    tech2.style.display = "block"
-    tech3.style.display = "block"
-    preference.style.display = "inline"
-    editTech1.style.display = "none"
-    editTech2.style.display = "none"
-    editTech3.style.display = "none"
-    editPreference.style.display = "none"
-}
-
-function menuToggle(){
-    const toggleMenu = document.querySelector('.menu');
-    toggleMenu.classList.toggle('active');
-  }
