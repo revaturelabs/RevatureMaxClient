4e9d6c24923f0ffa72e96a3886016ddbfe68fc88
mannyhi <mannyhinn@gmail.com>
2021-07-29 15:02:06 -0400

    update password-change

--- password-change/password-change.js
+++ password-change/password-change.js
@@ -3,10 +3,10 @@ async function invalid() {
     let response = await fetch(url)
     let element = document.getElementById("invalid")
     if (await response.json() == "invalid"){
-        element.innerHTML += "Incorrect Password. Try Again."
+        element.innerHTML += "Old password incorrect"
     }
     else if (await response.json() == "unmatched"){
-        element.innerHTML += "Not matching. Try Again."
+        element.innerHTML += "Passwords do not match"
     }
 }
 
