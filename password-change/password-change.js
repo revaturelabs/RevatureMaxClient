9f41f1c0a169ccaba19a7643f353040904902991
mannyhi <mannyhinn@gmail.com>
2021-07-29 14:00:46 -0400

    update index and password-change

--- password-change/password-change.js
+++ password-change/password-change.js
@@ -2,6 +2,12 @@ async function invalid() {
     let url = 'http://localhost:5000/invalid'
     let response = await fetch(url)
     let element = document.getElementById("invalid")
+    if (await response.json() == "invalid"){
+        element.innerHTML += "Incorrect Password. Try Again."
+    }
+    else if (await response.json() == "unmatched"){
+        element.innerHTML += "Not matching. Try Again."
+    }
 }
 
 window.onload = function () {
