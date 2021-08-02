83bf89115658e1dd8be657ea0a75ad17e248b80e
mannyhi <mannyhinn@gmail.com>
2021-08-02 18:36:22 -0400

    update index and password-change

--- password-change/password-change.js
+++ password-change/password-change.js
@@ -5,9 +5,12 @@ async function invalid() {
     let msg = await response.json();
     if (msg == "invalid"){
         element.innerHTML += "Old password incorrect"
+        document.getElementById("oldpassword").style.border = "thick solid #F26925";
     }
     else if (msg == "unmatched"){
         element.innerHTML += "Passwords do not match"
+        document.getElementById("newpassword").style.border = "thick solid #F26925";
+        document.getElementById("retype").style.border = "thick solid #F26925";
     }
 }
 
